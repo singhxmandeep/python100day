0b221638ae582aaf6dd108999bde9c2047627992
Mandeep Singh <33735343-mandeepsingh71m@users.noreply.replit.com>
2024-06-22 19:57:43 +0000

    day06-variables and data types

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
+# Day 06 - Variables
+a = 1
+a1 = 5
+print(a + a1)
+
+b = "Mandeep"
+print(b)
+print("Type of a is " , type(a))
+print("Type of b is " , type(b))
+
+list1 = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list1)
+
+tuple1 = (("parrot", "sparrow"), ("Lion", "Tiger"))
+print(tuple1)
+
+dist1 = {"name":"Sakshi", "age":20, "canVote":True}
+
+# print(a+b)
+
 # Day -5- Comments and escape sequence characters.
-print("hey I am a good boy\nand\t\t a \"good boy")  #Hello my name is Mandeep
-print("Hey", 6, 7)
-print("hey", 6, 7, sep="~")
-print("hey", 6, 7, sep="~", end="009\n")
-print("Mandeep")
+# print("hey I am a good boy\nand\t\t a \"good boy")  #Hello my name is Mandeep
+# print("Hey", 6, 7)
+# print("hey", 6, 7, sep="~")
+# print("hey", 6, 7, sep="~", end="009\n")
+# print("Mandeep")
+
 '''
 Day 04- First Program
 print("Hello World!")
