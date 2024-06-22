73259a3c9ec9eaf3745930dcd274c2a40d339b82
Mandeep Singh <33735343-mandeepsingh71m@users.noreply.replit.com>
2024-06-22 19:17:43 +0000

    day04

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 
+# Day 04- First Program
+print("Hello World!")
+print("hello world")
+print(5)
+print("Bye")
+print(17 * 13)
 
 
 
-# Day 03
+# Day 03- What is pip
 # import pandas
 # import hashlib
 # import tensorflow
