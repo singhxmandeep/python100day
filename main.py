5d8a0b966d0e8c081adf0d6d8d4daa5c652108c6
Mandeep Singh <33735343-mandeepsingh71m@users.noreply.replit.com>
2024-06-22 19:38:59 +0000

    day05

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
-
-# Day 04- First Program
+# Day -5- Comments and escape sequence characters.
+print("hey I am a good boy\nand\t\t a \"good boy")  #Hello my name is Mandeep
+print("Hey", 6, 7)
+print("hey", 6, 7, sep="~")
+print("hey", 6, 7, sep="~", end="009\n")
+print("Mandeep")
+'''
+Day 04- First Program
 print("Hello World!")
 print("hello world")
 print(5)
 print("Bye")
 print(17 * 13)
 
-
+Multiline comments
+'''
 
 # Day 03- What is pip
 # import pandas
